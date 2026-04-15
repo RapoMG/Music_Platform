fccd55207e1d4987ff703741b41ec80a852faa50
RapoMG <rapomg@gmail.com>
2026-04-15 22:30:00 +0200

    fix: Updated settings and urls for MEDIA_ROOT and MEDIA_URL

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-import sys
+import os
 import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -159,3 +159,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -40,3 +40,8 @@ urlpatterns = [
     path('api/', include('apps.catalog.urls')),
 
 ]
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
